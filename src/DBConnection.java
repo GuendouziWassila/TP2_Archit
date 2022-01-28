796d3bd24988d1f80afd8cc0b370dee13dfeb66e
khaoulacheridi <khaoulacheridi1276@gmail.com>
2022-01-29 00:17:17 +0100

    Q2  le patron singleton dans la classe « DBConnection

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -8,16 +8,20 @@ public class DBConnection {
 		String url = "jdbc:mysql://localhost:3306/" + BDD;
 		String user = "root";
 		String passwd = "";
-	    private Connection conn;
-
+	    private  Connection conn;
+	    private static DBConnection instance;
+	    
 	   
-	    public DBConnection() throws SQLException {
+	    private DBConnection() throws SQLException {
 			conn=DriverManager.getConnection(url, user,passwd);
 		}
-
+ 
 	    
-	    public Connection getConn() {
-			return conn;
+	    
+	    public static DBConnection getInstance() throws SQLException {
+			if(instance==null) {instance = new DBConnection();}
+	    	 
+			return instance;
 		}
 
 
