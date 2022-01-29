ab32270e3246744d1ec8242cb699924a4bbe1efb
Imad Eddine LASSAKEUR <MessaoudOuinten@Messaoud.local>
2022-01-29 23:36:25 +0100

    Abstract Factory

--- src/AbsractFactory.java
+++ src/AbsractFactory.java
@@ -1,6 +1,12 @@
 public class AbsractFactory {
-    public Package getPackage(int bonus){
-        
-        return null;
+    public AbsractFactory(){}
+    public Package getPackage(TypePackage TP){
+        if(TP.equals(TypePackage.Standard)){
+			return new Standard();
+			
+		}else if(TP.equals(TypePackage.Premium)){
+			return new Premuim();
+		}
+        return new Illimité();
     }
 }
