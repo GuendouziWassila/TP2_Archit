a9848acecd90f4ec8317a52d8054ccb3d1327e22
khaoulacheridi <khaoulacheridi1276@gmail.com>
2022-02-06 13:58:15 +0100

    Q4 et Q5

---
+++ src/InterfaceEtudiantReposit.java
@@ -0,0 +1,6 @@
+
+public interface InterfaceEtudiantReposit {
+	public abstract void  add( InterfaceEtudiant e);
+	public abstract boolean Exists(int matricule);
+	public  abstract boolean Exists(String email);
+}
---
+++ src/InterfaceEtudiantService.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+public interface InterfaceEtudiantService {
+public ArrayList<InterfaceEtudiant> GetEtudiantParUniversitye();
+	
+	public ArrayList<InterfaceEtudiant> GetEtudiatparLivreEmprunte();
+}
