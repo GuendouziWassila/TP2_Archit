b5e82a33b51a9549b8a85c5527f706d2f53fb80b
Mehdi Taleb <mehditaleb72@gmail.com>
2022-02-06 11:12:47 +0100

    Taleb_Mehdi_1

--- src/EtudiantService.java
+++ src/EtudiantService.java
@@ -3,12 +3,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
 public class EtudiantService {
 	
 	
 	boolean inscription (int matricule, String nom, String prénom, String email,String pwd, int id_universite) throws SQLException	
 	{
-		EtudiantRepository StudRep= new EtudiantRepository();
+		I_EtudiantRepository StudRep = I_EtudiantRepository.StudRep; //Inversion de dépendance 
 	    UniversiteRepository UnivRep= new UniversiteRepository();
 	    Etudiant stud = new Etudiant(matricule, nom, prénom, email,pwd,id_universite);
 	    Universite univ=UnivRep.GetById(id_universite);
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,4 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class MainApp {
 
