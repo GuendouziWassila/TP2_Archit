ee420f0448578a1625ba2ced268c4b076709d9d4
merouane-snn <merouanesennou@gmail.com>
2022-02-08 23:58:32 +0100

    quest 7 et 8

--- src/UniversiteRepository.java
+++ src/UniversiteRepository.java
@@ -1,4 +1,3 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,26 +6,30 @@ import java.sql.Statement;
 
 public class UniversiteRepository implements IUnivRepo {
 	
+	private IJournal journal;
 	
+	public UniversiteRepository(IJournal comp) {
+		this.journal = comp;
+	}
+	
+	@Override
 	public Universite GetById(int universityId) throws SQLException {
-		
 
 		Connection connect=DBConnection.getConn(); 
 		Statement stmt = connect.createStatement();
-		System.out.println("LogBD : début recherche de id université dans la base de donnée");
+		AfficheDateClass.setSender("UniversiteRepository");
+		journal.outPut_Msg("LogBD : début recherche de id université dans la base de donnée");
 		
 		String sql = "select * from universite where id_universite="+ universityId;
 		ResultSet rs = stmt.executeQuery(sql);
-		rs.next();	
+		rs.next();
 		TypePackage p=TypePackage.valueOf(rs.getString(3));
 		Universite u = new Universite (rs.getInt(1),rs.getString(2),p);
 			
-		System.out.println("LogBD : université récupérée");
+		journal.outPut_Msg("LogBD : université récupérée");
 		
 		connect.close();
-		return u;	
-	
-		
+		return u;
 	}	
 	
 }
