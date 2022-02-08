9ce4c5fdc881209e41b6bf2e3e81fbd7763fb0b0
Mehdi Taleb <mehditaleb72@gmail.com>
2022-02-08 14:05:11 +0100

    Taleb_Mehdi_2

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,24 +4,16 @@ import java.sql.SQLException;
 
 public class DBConnection {
 	
-	private static DBConnection instance_unique = new DBConnection();
+	static String BDD = "tp2";
+	static String url = "jdbc:mysql://localhost:3306/" + BDD;
+	static String user = "root";
+	static String passwd = "";
 	
-	private DBConnection() {}
-  
-    public static DBConnection getDBConnection() {
-		return instance_unique;	 //DBConnection -> Singleton 
-	}
+    private static Connection conn = null;
     
-	String BDD = "tp2";
-	String url = "jdbc:mysql://localhost:3306/" + BDD;
-	String user = "root";
-	String passwd = "";
-	
-    private static Connection conn;
-    
-    public Connection getConn() throws SQLException{
+    public static Connection getConn() throws SQLException{
     	
-    	conn = DriverManager.getConnection(url, user,passwd);
+    	if(conn == null) conn = DriverManager.getConnection(url, user,passwd);
 		return conn;
 	}
 	
