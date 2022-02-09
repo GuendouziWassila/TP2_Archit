c67069667e6bd65559836719df673b15e971e54a
MerieMMoussaoui <88559393+MerieMMoussaoui@users.noreply.github.com>
2022-02-09 23:26:05 +0100

    Questions 20 et 21

---
+++ src/ControleurInscription.java
@@ -0,0 +1,17 @@
+
+
+public class ControleurInscription{
+	
+	public void InscriptionControle(){
+		
+	    IEtdSrv serv=new EtudiantService();
+	    
+		try {
+			
+			serv.inscription(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx", 1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
