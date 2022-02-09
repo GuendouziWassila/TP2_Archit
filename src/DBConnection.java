0e0931c549ee302c853374a930951777e801419d
MohamedSaid99-Dev <mohamed.assassi99@gmail.com>
2022-02-09 09:41:07 +0100

    3eme question

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -8,13 +8,25 @@ public class DBConnection {
 		String url = "jdbc:mysql://localhost:3306/" + BDD;
 		String user = "root";
 		String passwd = "";
-	    private Connection conn;
+	  private Connection conn;
+	  
+		static private DBConnection DataBase = new DBConnection();
+		
 
 	   
-	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
+	  public DBConnection(){
+
+			try {
+				conn=DriverManager.getConnection(url, user,passwd);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
 		}
 
+	    static public DBConnection getDB() {
+	    	return DataBase;
+	    }
 	    
 	    public Connection getConn() {
 			return conn;
