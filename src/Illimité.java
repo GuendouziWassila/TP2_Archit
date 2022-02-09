02426bfee5ab84374dc007ae840e18fb2d81f347
Ahmed-dev-pro <79461195+Ahmed-dev-pro@users.noreply.github.com>
2022-02-09 20:08:32 +0100

    Question 18

---
+++ src/Illimité.java
@@ -0,0 +1,34 @@
+
+public class Illimité extends Package {
+
+	public Illimité() {
+		// TODO Auto-generated constructor stub
+		super(TypePackage.Illimité);
+		
+	}
+
+	@Override
+	public void SetNbLivreAutorise(int nombre) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void SetNbLivrebonus(int nombre) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getNbrLivreAutorise() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNbrLivreBonus() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
