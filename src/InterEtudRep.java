0ff747972de8b613283813ff4f6972cb2ca02ec7
MerieMMoussaoui <88559393+MerieMMoussaoui@users.noreply.github.com>
2022-02-09 13:16:33 +0100

    Question 4 et 5

---
+++ src/InterEtudRep.java
@@ -0,0 +1,16 @@
+/* cette modification nous offre de multiples façons de déplacer le code étroitement couplé entre les composants logiciels, */
+/*ce qui rend les conceptions logicielles plus compréhensibles, flexibles et maintenables.*/
+/*donc on a implemente une interface IEtdRep pour la class EtudiantRepository 
+
+import java.sql.SQLException;
+
+public interface InterEtudRep{
+
+	boolean Exists(int matricule) throws SQLException;
+
+	boolean Exists(String email) throws SQLException;
+
+	
+	
+
+}
