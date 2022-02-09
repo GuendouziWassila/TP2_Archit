fb5e4105fe0b2e263ae34b072ab73d934aeba2ce
Imane-AOUANOUK <imaneaouanouk@gmail.com>
2022-02-09 22:44:55 +0100

    réponse question 12 et 13 et 14

--- src/ForfaitLimite.java
+++ src/ForfaitLimite.java
@@ -1,15 +1,14 @@
 
 
-public class ForfaitLimite extends Etudiant {
+public class ForfaitLimite {
 
 	public ForfaitLimite(int matricule, String nom, String prenom, String email, String pwd, int id_universite) {
-		super(matricule, nom, prenom, email, pwd, id_universite);
+		super();
 	}
 	
 	public void Add_Nbr_Books () {
 	}
 
-	@Override
 	public void nbr_book_authorized() {
 		// TODO Auto-generated method stub
 		
