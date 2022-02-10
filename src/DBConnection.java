2993e208bb30c7e73af8a47747ac6851676f4ce4
arslanezem <zemmouriarslane@gmail.com>
2022-02-10 23:48:57 +0100

    Questions 1 et 2

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,24 +3,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	   
-		String BDD = "nomBD";
-		String url = "jdbc:mysql://localhost:3306/" + BDD;
-		String user = "root";
-		String passwd = "";
-	    private Connection conn;
-
-	   
-	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
-		}
-
-	    
-	    public Connection getConn() {
-			return conn;
-		}
-
-
-		
 	
+	private String BDD = "nomBD";
+	private String url = "jdbc:mysql://localhost:3306/" + BDD;
+	private String user = "root";
+	private String passwd = "";		
+	private Connection conn;
+
+	private DBConnection() {}
+	
+	private static DBConnection instance;
+		    
+	public synchronized Connection getConn() throws SQLException {
+		conn = DriverManager.getConnection(url, user, passwd);
+		return conn;
+	}
+
+	public static DBConnection getInstance() {		
+	  	if (instance == null) {
+	   		instance = new DBConnection();
+	   	}
+	   	return instance;
+	}
+			
 }
