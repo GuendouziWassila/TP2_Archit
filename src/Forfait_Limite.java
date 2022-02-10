7c35cbe48372d49fee036da686f8a6bba32e1101
Kaci <kachan23dz@gmail.com>
2022-02-10 21:28:06 +0100

    question_17

---
+++ src/Forfait_Limite.java
@@ -0,0 +1,11 @@
+
+public abstract class Forfait_Limite  extends Etudiant {
+
+	public Forfait_Limite(int matricule, String nom, String prenom, String email, String pwd, int id_universite) {
+		super(matricule, nom, prenom, email, pwd, id_universite);
+	}
+	
+	public abstract void Add_Nbr_Books ();
+
+	
+}
---
+++ src/Standard.java
@@ -0,0 +1,23 @@
+
+public class Standard  extends Forfait_Limite{
+
+	public Standard(int matricule, String nom, String prenom, String email, String pwd, int id_universite) {
+		super(matricule, nom, prenom, email, pwd, id_universite);
+	}
+
+	@Override
+	public void Add_Nbr_Books() {
+		setNbLivreMensuel_Autorise(15);	
+		
+	}
+
+	@Override
+	public void NbrBookAuthorized() {
+		setNbLivreMensuel_Autorise(10);	
+	}
+	
+	
+	
+	
+	
+}
