24c556258dfd403501eba32ad19867fb571547f6
Kaci <kachan23dz@gmail.com>
2022-02-10 21:47:16 +0100

    question_22

---
+++ src/ControleurInscription.java
@@ -0,0 +1,20 @@
+
+public class ControleurInscription {
+	
+	public void InscriptionControle(){
+		
+	IEtudService serv = new EtudiantService();
+    
+	try {
+	
+		serv.inscription(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx", 1);
+	
+	} catch (Exception e) {
+		
+		e.printStackTrace();
+	}
+	
+}
+
+	
+}
