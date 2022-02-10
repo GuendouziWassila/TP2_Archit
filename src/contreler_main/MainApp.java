70f286cca1bd85db8f1f6fa84ec12a3a13c5afa6
rmc <rmc@example.com>
2022-02-10 19:57:35 +0100

    finel commit without the questions 16 and 18  question

--- src/contreler_main/MainApp.java
+++ src/contreler_main/MainApp.java
@@ -1,2 +1,19 @@
-package contreler_main;
+/*package contreler_main;
 
+
+import service_pack.EtudiantService;
+public class MainApp {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		EtudiantService serv=new EtudiantService();
+		try {
+			serv.inscription(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx", 1);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}*/
