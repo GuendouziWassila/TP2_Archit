d9917d4cfabafab90869484f1a061bf68288800b
Dell <ghoula567@gmail.com>
2022-11-20 22:18:47 +0100

    Question 2

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,24 +3,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	   
-		String BDD = "nomBD";
+
+		String BDD = "gestion";
 		String url = "jdbc:mysql://localhost:3306/" + BDD;
 		String user = "root";
 		String passwd = "";
 	    private Connection conn;
+	    private static DBConnection instance;
 
-	   
-	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
+
+			
+	    private DBConnection() throws SQLException {
+			conn = DriverManager.getConnection(url, user,passwd);
+
 		}
 
-	    
+
 	    public Connection getConn() {
 			return conn;
 		}
 
+	    public static DBConnection getInstance() throws SQLException {
+	    	if(instance == null) {
+	    		instance = new DBConnection();
+	    	}
+	    	return instance;
+	    }
 
-		
+
+
 	
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,19 +1,27 @@
 import java.sql.SQLException;
+import java.sql.*;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
 
 		EtudiantService serv=new EtudiantService();
-		try {
-			serv.inscription(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx", 1);
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		serv.inscription(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx", 1);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		
 	}
 
+}
+
+
+		
+
+
+			
+		
+	}
 }
