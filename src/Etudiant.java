cfd7cce5d28bab0de25a44d26279c058f1d3cb42
Dell <ghoula567@gmail.com>
2022-11-23 22:31:56 +0100

    Question 12

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,4 +1,7 @@
+import java.sql.SQLException;
+
 public class Etudiant {
+
 		private int matricule;
 	    private String nom;
 	    private String prenom;
@@ -7,14 +10,14 @@ public class Etudiant {
 	    private int nbLivreMensuel_Autorise;
 	    private int nbLivreEmprunte;
 	    private int id_universite;
+
+
 	    
-	   
 		public Etudiant(int matricule, String nom, String prenom, String email,String pwd, int id_universite) {
-			
+
 			this.matricule = matricule;
 			this.nom = nom;
 			this.prenom = prenom;
-	
 			if(email != null && email.length() != 0)
 		    {
 				this.email = email;
@@ -34,7 +37,6 @@ public class Etudiant {
 		public void setNom(String nom) {
 			this.nom = nom;
 		}
-		
 		public String getPrenom() {
 			return prenom;
 		}
@@ -71,6 +73,20 @@ public class Etudiant {
 		public void setPwd(String pwd) {
 			this.pwd = pwd;
 		}
-		
-		
+
+		public void giveBonus(Universite univ) throws SQLException {
+
+			if (univ.getPack() == TypePackage.Standard)
+		     {
+				this.nbLivreMensuel_Autorise += 5 ;
+		     }
+		     else if (univ.getPack() == TypePackage.Premium)
+		     {
+		    	 this.nbLivreMensuel_Autorise += 10 ;
+		     }                           
+
+		}
+
+
+
 	    }
