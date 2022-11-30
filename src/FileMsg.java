90f18c3de2b95dc01f7455b397aceed53c012b21
Naz <nazimmizo7@gmail.com>
2022-11-30 17:57:00 +0100

    Fix FileMsg.java

--- src/FileMsg.java
+++ src/FileMsg.java
@@ -4,11 +4,11 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;  
 import java.io.PrintStream;  
 
-public class FileMsg implements IJournal    {
+public class FileMsg implements IJournal  {
 
     //File file = new File("C:/Users/Asus-U/Desktop/Archi2/TP2_Archit/src/A.txt");
     
-    PrintStream o;
+    static PrintStream o;
    // FileMsg(PrintStream o){
      // this.o =o;
                         //  }
@@ -22,20 +22,23 @@ public class FileMsg implements IJournal    {
     public void outPut_Msg(String message) {
        
     try {
+        if(o == null){
         o = new PrintStream(new File("C:/Users/Asus-U/Desktop/Archi2/TP2_Archit/src/A.txt"));
-
+        }
+        else {
         PrintStream console = System.out;
        System.setOut(o);
        System.out.println(message);
-       System.setOut(console);
-
-    } catch (FileNotFoundException e) {
+       System.setOut(console);}
+        }
+    
+     catch (FileNotFoundException e) {
     
         e.printStackTrace();
-    }
-       
+    }}}
+
      
-    }
+    
    
     
-}
+
