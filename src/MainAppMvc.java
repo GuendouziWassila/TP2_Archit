42831f5166085261acf1d68590f1fe2c24e2ba14
serine <larouisirine@gmail.com>
2022-12-06 19:02:01 +0100

    creation de MainAppMvc

--- src/MainAppMvc.java
+++ src/MainAppMvc.java
@@ -1,33 +1,34 @@
-/* import java.sql.SQLException;
+import java.sql.SQLException;
 
-public class MainApp {
+public class MainAppMvc {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		AfficheComposite comp = new AfficheComposite();
-
+	public static void main(String[] args) throws SQLException {
+AfficheComposite comp = new AfficheComposite();
+		
 		IJournal Ecran = new AfficheEcran();
 		IJournal Fichier = new AfficheFichier();
 		IJournal DateClass = new AfficheDateClass();
-
+		
 		comp.ajouter(Ecran);
 		comp.ajouter(Fichier);
 		comp.ajouter(DateClass);
-
+		
 		IEtudiantREPO StudRep = new EtudiantRepository(comp);
 		IUnivREPO UnivRep= new UniversiteRepository(comp);
 		EtudiantService serv=new EtudiantService(StudRep, UnivRep, comp);
-		
+
 		IEtudiant etudiant = new Etudiant(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx", 1);
+
+		ViewInscription v = new ViewInscription();
+		v.Show_Inscription();
 		try {
 			serv.inscription(etudiant);
-			//System.out.println("hello world");
-			
+
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
+			
 		}
 	}
 
-} */
+}
